a72ddc0435afc6a738ce7649a0167808e760a72c
algernon-A <aeneas@aloku.net>
2022-10-09 15:44:26 +1100

    Add AutoTabstrip class.

--- UI/UITabstrips.cs
+++ UI/UITabstrips.cs
@@ -5,6 +5,7 @@
 
 namespace AlgernonCommons.UI
 {
+    using System.Linq;
     using ColossalFramework.UI;
     using UnityEngine;
 
@@ -14,7 +15,7 @@ namespace AlgernonCommons.UI
     public static class UITabstrips
     {
         /// <summary>
-        /// Adds a tabstrip panel.
+        /// Adds a tabstrip panel to the specified parent.
         /// </summary>
         /// <param name="parent">Parent component.</param>
         /// <param name="posX">Relative X postion.</param>
@@ -22,11 +23,28 @@ namespace AlgernonCommons.UI
         /// <param name="width">Tabstrip panel width.</param>
         /// <param name="height">Tabstrip panel height (including tabs).</param>
         /// <param name="container">Tabstrip tab container instance.</param>
-        /// <returns>New tabstrip.</returns>
-        public static UITabstrip AddTabStrip(UIComponent parent, float posX, float posY, float width, float height, out UITabContainer container)
+        /// <param name="tabHeight">Tabstrip tab height (default 25f).</param>
+        /// <returns>New tabstrip of the specified type.</returns>
+        public static UITabstrip AddTabstrip(UIComponent parent, float posX, float posY, float width, float height, out UITabContainer container, float tabHeight = 25f) =>
+            AddTabstrip<UITabstrip>(parent, posX, posY, width, height, out container, tabHeight);
+
+        /// <summary>
+        /// Adds a tabstrip panel to the specified parent.
+        /// </summary>
+        /// <typeparam name="TTabstrip">Tabstrip type.</typeparam>
+        /// <param name="parent">Parent component.</param>
+        /// <param name="posX">Relative X postion.</param>
+        /// <param name="posY">Relative Y position.</param>
+        /// <param name="width">Tabstrip panel width.</param>
+        /// <param name="height">Tabstrip panel height (including tabs).</param>
+        /// <param name="container">Tabstrip tab container instance.</param>
+        /// <param name="tabHeight">Tabstrip tab height.</param>
+        /// <returns>New tabstrip of the specified type.</returns>
+        public static TTabstrip AddTabstrip<TTabstrip>(UIComponent parent, float posX, float posY, float width, float height, out UITabContainer container, float tabHeight)
+            where TTabstrip : UITabstrip
         {
             // Basic setup.
-            UITabstrip tabStrip = parent.AddUIComponent<UITabstrip>();
+            TTabstrip tabStrip = parent.AddUIComponent<TTabstrip>();
             tabStrip.relativePosition = new Vector2(posX, posY);
             tabStrip.width = width;
             tabStrip.height = height;
@@ -35,9 +53,9 @@ namespace AlgernonCommons.UI
             // Tab container (the panels underneath each tab).
             UITabContainer tabContainer = parent.AddUIComponent<UITabContainer>();
             tabContainer.name = "TabContainer";
-            tabContainer.relativePosition = new Vector2(posX, posY + 30f);
+            tabContainer.relativePosition = new Vector2(posX, posY + tabHeight + 5f);
             tabContainer.width = width;
-            tabContainer.height = height;
+            tabContainer.height = height - tabHeight - 5f;
             tabContainer.clipChildren = false;
             tabStrip.tabPages = tabContainer;
 
@@ -48,17 +66,17 @@ namespace AlgernonCommons.UI
         /// <summary>
         /// Adds a text-based tab to a UI tabstrip.
         /// </summary>
-        /// <param name="tabStrip">UIT tabstrip to add to.</param>
+        /// <param name="tabstrip">UIT tabstrip to add to.</param>
         /// <param name="tabName">Name of this tab.</param>
         /// <param name="tabIndex">Index number of this tab.</param>
         /// <param name="button">Tab button instance.</param>
         /// <param name="width">Tab width.</param>
         /// <param name="autoLayout">Default autoLayout setting.</param>
         /// <returns>UIPanel instance for the new tab panel.</returns>
-        public static UIPanel AddTextTab(UITabstrip tabStrip, string tabName, int tabIndex, out UIButton button, float width = 170f, bool autoLayout = false)
+        public static UIPanel AddTextTab(UITabstrip tabstrip, string tabName, int tabIndex, out UIButton button, float width = 170f, bool autoLayout = false)
         {
             // Create tab.
-            UIButton tabButton = tabStrip.AddTab(tabName);
+            UIButton tabButton = tabstrip.AddTab(tabName);
 
             // Sprites.
             tabButton.normalBgSprite = "SubBarButtonBase";
@@ -70,11 +88,14 @@ namespace AlgernonCommons.UI
             // Tooltip.
             tabButton.tooltip = tabName;
 
-            // Force width.
-            tabButton.width = width;
+            // Force width if this isn't an auto panel.
+            if (!(tabstrip is AutoTabstrip))
+            {
+                tabButton.width = width;
+            }
 
             // Get tab root panel.
-            UIPanel rootPanel = tabStrip.tabContainer.components[tabIndex] as UIPanel;
+            UIPanel rootPanel = tabstrip.tabContainer.components[tabIndex] as UIPanel;
 
             // Panel setup.
             rootPanel.autoLayout = autoLayout;
@@ -90,7 +111,7 @@ namespace AlgernonCommons.UI
         /// <summary>
         /// Adds an icon-based tab to a UI tabstrip.
         /// </summary>
-        /// <param name="tabStrip">UIT tabstrip to add to.</param>
+        /// <param name="tabstrip">UIT tabstrip to add to.</param>
         /// <param name="tabName">Name of this tab.</param>
         /// <param name="tabIndex">Index number of this tab.</param>
         /// <param name="iconNames">Icon sprite names.</param>
@@ -98,13 +119,13 @@ namespace AlgernonCommons.UI
         /// <param name="width">Tab width.</param>
         /// <param name="autoLayout">Default autoLayout setting.</param>
         /// <returns>UIPanel instance for the new tab panel.</returns>
-        public static UIPanel AddIconTab(UITabstrip tabStrip, string tabName, int tabIndex, string[] iconNames, string[] atlasNames, float width = 120f, bool autoLayout = false)
+        public static UIPanel AddIconTab(UITabstrip tabstrip, string tabName, int tabIndex, string[] iconNames, string[] atlasNames, float width = 120f, bool autoLayout = false)
         {
             // Layout constants.
             const float TabIconSize = 23f;
 
             // Create tab.
-            UIPanel rootPanel = AddTextTab(tabStrip, tabName, tabIndex, out UIButton button, width, autoLayout);
+            UIPanel rootPanel = AddTextTab(tabstrip, tabName, tabIndex, out UIButton button, width, autoLayout);
 
             // Clear button text.
             button.text = string.Empty;
