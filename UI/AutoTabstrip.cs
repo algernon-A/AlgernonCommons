f2ffa175774fdbc1076480003189fb54394eb744
algernon-A <aeneas@aloku.net>
2022-10-09 15:48:42 +1100

    Update header.

--- UI/AutoTabstrip.cs
+++ UI/AutoTabstrip.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="UITabstrips.cs" company="algernon (K. Algernon A. Sheppard)">
+﻿// <copyright file="AutoTabstrip.cs" company="algernon (K. Algernon A. Sheppard)">
 // Copyright (c) algernon (K. Algernon A. Sheppard). All rights reserved.
 // Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 // </copyright>
